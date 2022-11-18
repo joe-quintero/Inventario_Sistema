316a55a99e73257252389966c8ec86bc6e843885
Joe Quintero <joedarwin1998@gmail.com>
2022-11-18 17:30:11 -0400

    Cierre de sesion para todos los modulos

--- pages/11-edicion_productos.php
+++ pages/11-edicion_productos.php
@@ -323,17 +323,4 @@ if ($resultado) {
 
 </div>
 
-<!-- jQuery -->
-<script src="js/jquery.min.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Metis Menu Plugin JavaScript -->
-<script src="js/metisMenu.min.js"></script>
-
-<!-- Custom Theme JavaScript -->
-<script src="js/startmin.js"></script>
-
-</body>
-</html>
+<?php include '../templates/script.php'; //JavaScript
--- pages/6-proveedores.php
+++ pages/6-proveedores.php
@@ -239,20 +239,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 </div>
 
-<!--cierre de la conexion-->
-<?php mysqli_close($db); ?>
-
-<!-- jQuery -->
-<script src="js/jquery.min.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Metis Menu Plugin JavaScript -->
-<script src="js/metisMenu.min.js"></script>
-
-<!-- Custom Theme JavaScript -->
-<script src="js/startmin.js"></script>
-
-</body>
-</html>
+<?php include '../templates/script.php'; //JavaScript
