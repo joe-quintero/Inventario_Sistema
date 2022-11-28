0fd749e835ec81b2d481e4136552deb2ae5cabc8
Joe Quintero <joedarwin1998@gmail.com>
2022-11-28 10:49:18 -0400

    Editando de gitignore la carpeta vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joe/inventario_sistema',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3c4a8f09b3c06a40fb2a7ed9ef769e4d50aef0c3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joe/inventario_sistema' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3c4a8f09b3c06a40fb2a7ed9ef769e4d50aef0c3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
